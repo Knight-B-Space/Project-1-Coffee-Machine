8946e265fdfd14dd223e777cbdc9230462e580b2
Knight <knightb0110@gmail.com>
2022-05-17 23:24:28 +1000

    end

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from Materials import MENU, resources
-order = input("What would you like? (espresso/latte/cappuccino):")
-machine_state = True
-
-if order == "report":
-    for i in resources:
-        print(f"{i}: {resources[i]}")
 
 
-rest_of_resources = resources
-order_ingredients = MENU[order]["ingredients"]
+def report():
+    print("Current resources are:\n")
+    for i in rest_of_resources:
+        print(f"{i}: {rest_of_resources[i]}")
 
 
 def check_resource():
@@ -26,5 +22,35 @@ def process_coins():
     n_nickles = int(input("How many nickles?"))
     n_pennies = int(input("How many pennies?"))
     quarters, dimes, nickles, pennies = (0.25, 0.1, 0.05, 0.01)
-    total_money = sum(n_quarter*quarters, n_dimes*dimes, n_nickles*nickles, n_pennies*pennies)
+    total_money = n_quarter*quarters + n_dimes*dimes + n_nickles*nickles + n_pennies*pennies
     return total_money
+
+
+machine_state = True
+rest_of_resources = resources
+while machine_state:
+    order = input("What would you like? (espresso/latte/cappuccino):")
+    if order == "report":
+        report()
+    elif order == "off":
+        machine_state = False
+    else:
+        order_ingredients = MENU[order]["ingredients"]
+        order_cost = MENU[order]["cost"]
+        resource_status = check_resource()
+        if resource_status:
+            insert_money = process_coins()
+            if insert_money < order_cost:
+                print("Sorry, that's not enough money. Money refunded")
+            elif insert_money == order_cost:
+                for k in MENU[order]["ingredients"]:
+                    rest_of_resources[k] -= MENU[order]["ingredients"][k]
+                    print(f"Here is your {order}. Enjoy!")
+                    report()
+            else:
+                for k in MENU[order]["ingredients"]:
+                    rest_of_resources[k] -= MENU[order]["ingredients"][k]
+                print(f"Here is your {order}. Enjoy!\n")
+                report()
+                change = round(insert_money - order_cost, 2)
+                print(f"Here is ${change} dollars in change")
